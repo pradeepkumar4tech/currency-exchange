faa1b74508fd34485a7e7f15b446da96c4064e3b
pradeepkumar4tech <pradeepkumar4tech.522@gmail.com>
2022-01-15 00:00:34 +0530

    first commit

---
+++ target/test-classes/com/springboot/currencyexchangeservice/data.sql
@@ -0,0 +1,4 @@
+insert into CURRENCY_EXCHANGE (currency_id,currency_from,currency_to,currency_conversion,environment) values (1000,'USD','INR','65','');
+insert into CURRENCY_EXCHANGE (currency_id,currency_from,currency_to,currency_conversion,environment) values (1001,'EUR','INR','65','');
+insert into CURRENCY_EXCHANGE (currency_id,currency_from,currency_to,currency_conversion,environment) values (1002,'AUD','INR','65','');
+commit;
